03b9caa5e6ddc76d7f7a5ea56f0ad1e117bf3523
h0useofdupree <joel.riekemann@gmail.com>
2023-06-07 23:15:57 +0200

    Changes and Added README

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,14 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  lazy = false,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
 }
