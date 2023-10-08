a26d41acfb6cff1fa915c08b6ed0e34542b26a45
h0useofdupree <joel.riekemann@gmail.com>
2023-10-08 17:58:28 +0200

    Minor adjustments

--- plugins/user.lua
+++ plugins/user.lua
@@ -10,13 +10,51 @@ return {
   --   end,
   -- },
   {
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  lazy = false,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  }
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+  },
+  {
+    "lambdalisue/suda.vim",
+    lazy = false
+  },
+  {
+    'lervag/vimtex',
+    lazy = false
+  },
+  {
+    "jackMort/ChatGPT.nvim",
+    cmd = "ChatGPT",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("chatgpt").setup()
+    end,
+  },
+  {
+    'donRaphaco/neotex',
+    ft = "tex"
+  },
+  {
+    'declancm/cinnamon.nvim',
+    config = function()
+      require("cinnamon").setup()
+    end,
+  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   event = "InsertEnter",
+  --   config = function()
+  --     require("copilot").setup({})
+  --   end,
+  -- }
 }
